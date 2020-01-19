314c45ab39bd7378430434158bbc3907b6d236fe
brandongatlin <brandongatlin1981@me.com>
2020-01-19 01:07:07 -0600

    added top 5 scoreboard

--- trivia-project/game/views.py
+++ trivia-project/game/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Question, Score
 from django.db import connection
 from random import randint, shuffle
-from pprint import PrettyPrinter
 
 
 def index(request):
-    return render(request, 'game/index.html')
+    top_five = Score.objects.order_by('-score')[:5].values()
+    top_five = list(top_five)
+    return render(request, 'game/index.html', {'top_five': top_five})
 
 def gameboard(request, category_id=1):
-    pp = PrettyPrinter(indent=4)
-
     base_q = Question.objects.filter(category=category_id)
     count = base_q.count()
     limit = 5
@@ -20,10 +19,7 @@ def gameboard(request, category_id=1):
     questions = base_q.order_by('id')[offset:offset+limit].values()
     questions = list(questions)
     shuffle(questions)
-    for dict in questions:
-        pp.pprint(dict)
     return render(request, 'game/gameboard.html', {'questions': questions})
-
 
 def gameover(request):
     return render(request, 'game/gameover.html')
